2dda702d87099cc0a32052e93eea71059b9d3062
iEdwinT <edwin_torres@icloud.com>
2020-02-26 20:31:08 -0500

    Get data from the github API and update state

--- src/App.js
+++ src/App.js
@@ -1,15 +1,32 @@
 import React, { Component } from "react";
 import "./App.css";
 
+const githubUserInfoURL = (githubUserName) =>
+	`https://api.github.com/users/${githubUserName}`;
+
 class App extends Component {
+	state = {
+		user: {},
+		active: false
+	};
 
-  handleToggle(event) {
-    console.log("was clicked")
-  }
+	handleToggle = (event) => {
+		console.log("was clicked");
+		fetch(githubUserInfoURL("davegregg"))
+			.then((response) => response.json())
+			.then((responseBody) => {
+				console.log(responseBody);
+				this.setState({ user: responseBody });
+			});
+	}
 
-  render() {
-    return <button onClick={this.handleToggle}>Toggle User View</button>;
-  }
+	render() {
+		return (
+			<button onClick={this.handleToggle}>
+				Toggle User View
+			</button>
+		);
+	}
 }
 
 export default App;
