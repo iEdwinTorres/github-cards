401aa5e41fd5d51d1dcedc43dc1c6f9e071e9fa2
iEdwinT <edwin_torres@icloud.com>
2020-02-26 19:32:21 -0500

    Create a button with a basic event handler

--- src/App.js
+++ src/App.js
@@ -1,26 +1,15 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from "react";
+import "./App.css";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+
+  handleToggle(event) {
+    console.log("was clicked")
+  }
+
+  render() {
+    return <button onClick={this.handleToggle}>Toggle User View</button>;
+  }
 }
 
 export default App;
